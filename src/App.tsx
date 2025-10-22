e3f2f8d1d77f8e991a819ed2940482c45c2338b0
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-10-21 23:04:55 -0300

    Introducing AppContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Document } from './components/Document';
+import { createContext, useState } from 'react';
+import Document from './components/Document';
+import Development from './components/Development';
 import type { THeader } from './components/Header';
 import type { TSection } from './components/Section';
 
@@ -29,11 +31,23 @@ const sampleSections: TSection[] = [
   }
 ];
 
+export const AppContext = createContext({
+  header: sampleHeader,
+  sections: sampleSections
+})
+
 const App = () => {
-  const header = sampleHeader;
-  const sections = sampleSections;
+  const [header, setHeader] = useState<THeader>(sampleHeader);
+  const [sections, setSections] = useState<TSection[]>(sampleSections);
+
+  function handleChangeContent() { }
+  function handleChangeStyle() { }
+
   return (
-    <Document header={header} sections={sections} />
+    <AppContext.Provider value={{ header, sections }}>
+      <Development onChangeContent={handleChangeContent} onChangeStyle={handleChangeStyle} />
+      <Document />
+    </AppContext.Provider>
   );
 };
 
--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,19 +1,16 @@
 import { Header } from './Header';
 import { Section } from './Section';
-import type { THeader } from './Header';
-import type { TSection } from './Section';
+import { AppContext } from '../App';
+import { useContext } from 'react';
 
-type TDocument = {
-  header: THeader;
-  sections: TSection[];
-}
+const Document = () => {
+  const { header, sections } = useContext(AppContext);
+  return (
+    <div className="font-sans w-[210mm] h-[297mm] mx-auto p-3 bg-white shadow-lg text-gray-900 leading-relaxed">
+      <Header {...header} />
+      {sections.map((s, index) => <Section key={index} {...s} />)}
+    </div>
+  )
+};
 
-const Document = ({header, sections = []}: TDocument) => (
-  <div className="font-sans w-[210mm] h-[297mm] mx-auto p-3 bg-white shadow-lg text-gray-900 leading-relaxed">
-    <Header {...header}/>
-    {sections.map((s, index) => <Section key={index} {...s}/>)}
-  </div>
-);
-
-export { Document };
-export type { TDocument };
+export default Document;
