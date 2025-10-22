88ce3ac9897aaf260eb2786f1ff1010dbbd4fcd2
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-10-21 23:00:41 -0300

    Adding keys to Header contacts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,8 +17,8 @@ const Header = ({ name, contacts = [] }: THeader) => (
     {contacts.length > 0 && (
       <div className="flex justify-center space-x-4 text-sm text-gray-700">
         {
-          contacts.map(({icon, label, link}) => (
-            <span className="flex items-center space-x-1">
+          contacts.map(({ icon, label, link }, index) => (
+            <span className="flex items-center space-x-1" key={index}>
               {icon && icon}
               <a href={isEmail(link) ? `mailto:${link}` : link} className="text-gray-600 hover:underline">{label}</a>
             </span>
