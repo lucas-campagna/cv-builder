78257b21eff25556ae0627fb870c74c3cc49eff2
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-10-28 21:18:01 -0300

    Document integrated with parser

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,15 +1,12 @@
-import { Header } from './Header';
-import { Section } from './Section';
-import { AppContext } from '../App';
-import { useContext } from 'react';
 import { A4 } from "@/constants";
+import useDynamicComponents from "@/hooks/useDynamicComponents";
+
 
 const Document = () => {
-  const { header, sections } = useContext(AppContext);
+  const { Cv } = useDynamicComponents();
   return (
     <div className={"doc font-sans mx-auto p-3 bg-white shadow-lg text-gray-900 leading-relaxed"} style={A4}>
-      <Header {...header} />
-      {sections.map((s, index) => <Section key={index} {...s} />)}
+      <Cv />
     </div>
   )
 };
