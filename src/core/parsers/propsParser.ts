6cd5fb53208914ee34b032d2f44bd2cbed7ea443
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-11-04 21:51:25 -0300

    Removing usedKeys in props parser

--- src/core/parsers/propsParser.ts
+++ src/core/parsers/propsParser.ts
@@ -9,16 +9,11 @@ const parse = (
     const match = str.match(varRegEx);
     return match && match[0] === str;
   };
-  const usedKeys = new Set();
   const parse = <T>(target: T): T =>
     typeof target === "string"
       ? isPureVariable(target)
-        ? (() => {
-            if (values[target.substring(1)]) usedKeys.add(target.substring(1));
-            return values[target.substring(1)] as T;
-          })()
+        ? (values[target.substring(1)] as T)
         : (target.replace(varRegEx, (_, p1) => {
-            if (values[p1]) usedKeys.add(p1);
             const value = String(values[p1] || "");
             return value.startsWith("$")
               ? (parse(value) as string)
@@ -31,15 +26,7 @@ const parse = (
           Object.entries(target).map(([k, v]) => [k, parse(v)])
         ) as T)
       : target;
-  const parsedTarget = parse(target);
-  const unusedKeys = Object.fromEntries(
-    Object.entries(values).filter(([k]) => !usedKeys.has(k))
-  );
-  return {
-    // TODO: Not sure about unusedKeys here
-    ...unusedKeys,
-    ...parsedTarget,
-  };
+  return parse(target);
 };
 
 export default parse;
