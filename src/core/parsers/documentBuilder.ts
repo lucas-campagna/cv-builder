ae11c03075cd0f6baa3925ea534b939788cfdd5a
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-11-04 21:28:41 -0300

    Typing

--- src/core/parsers/documentBuilder.ts
+++ src/core/parsers/documentBuilder.ts
@@ -1,11 +1,11 @@
 import buildComponent from "./componentBuilder";
 import parseYaml from "./yamlParser";
 import render from "./renderer";
-import type { ComponentProps, PropsType } from "./types";
+import type { PropsType, SetOfComponents } from "./types";
 import type React from "react";
 
 const build = (
-  code: string | Record<string, ComponentProps>,
+  code: string | SetOfComponents,
   entry: string = "document"
 ): ((_?: PropsType) => React.ReactNode) => {
   const component = buildComponent(
