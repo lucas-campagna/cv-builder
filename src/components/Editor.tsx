2d020dfaf8a4bb3d946eb3de7d6198fdf3ac49b4
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-11-06 20:04:01 -0300

    Add initialText to Editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,20 +2,14 @@ import { memo } from "react";
 import YamlEditor from "@focus-reactive/react-yaml";
 import type { YamlData } from "@/utils/parseDynamicComponents";
 
-type Editor = Omit<React.ComponentProps<"textarea">, 'onChange'> & {
+type Editor = {
+  initialText: string;
   onChange: (_: YamlData) => void;
-  title?: string;
-}
-const Editor = ({ onChange, title }: Editor) => {
-  return (
-    <div className="bg-white h-[calc(100vh-20px)] flex flex-col rounded-sm ">
-      {title &&
-        <div className="text-muted-foreground px-1 focus-visible:border-0 focus-visible:ring-ring-0 focus-visible:ring-0">{title}</div>}
-      <YamlEditor
-        onChange={({ json }) => onChange(json)}
-      />
-    </div>
-  )
 };
+const Editor = ({ initialText, onChange }: Editor) => (
+  <div className="h-full flex flex-col">
+    <YamlEditor text={initialText} onChange={({ json }) => onChange(json)} />
+  </div>
+);
 
 export default memo(Editor);
