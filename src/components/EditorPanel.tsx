fabc006967a8060ed399bcf55e8689231906311a
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-11-06 22:20:11 -0300

    Simplify EditorPanel

--- src/components/EditorPanel.tsx
+++ src/components/EditorPanel.tsx
@@ -6,17 +6,8 @@ import Explorer from "./Explorer";
 import { useExplorer } from "@/hooks/useExplorer";
 
 const EditorPanel = () => {
-  const [content, setContent] = useState<YamlData>({});
-  const [structure, setStructure] = useState<YamlData>({});
   const { update } = useDynamicComponents();
   const { state } = useExplorer();
-  // const { toggleDebug } = useAppState();
-
-  const handleContentChange = (data: YamlData) => {
-    console.log("Content changed:", data);
-    setContent(data);
-    update({ ...structure, ...data });
-  };
 
   const initialText = useMemo(
     () =>
@@ -28,7 +19,10 @@ const EditorPanel = () => {
     <div className="h-full bg-white rounded-sm flex h-full">
       <Explorer />
       <div className="flex-1 overflow-auto">
-        <Editor initialText={initialText} onChange={handleContentChange} />
+        <Editor
+          initialText={initialText}
+          onChange={(data: YamlData) => update(data)}
+        />
       </div>
     </div>
   );
