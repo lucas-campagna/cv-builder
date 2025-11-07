754f407543d9dd973aeada41fdeeb37f9ca77276
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-11-07 14:41:01 -0300

    Reformat Document component

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,58 +1,18 @@
-import { useEffect, useRef, useState } from "react";
 import { A4 } from "@/constants";
 import useAppState from "@/hooks/useAppState";
 import useDynamicComponents from "@/hooks/useDynamicComponents";
 import { renderToString } from "react-dom/server";
 
-const [width, height] = [parseInt(A4.width), parseInt(A4.height)];
-const a4Ratio = width / height;
-
 const Document = () => {
   const { isDebugging } = useAppState();
   const { Document } = useDynamicComponents();
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [scale, setScale] = useState(1);
-
-  useEffect(() => {
-    const container = containerRef.current;
-    if (!container?.children?.[0]) return;
-
-    const updateScale = () => {
-      const rect = container.children[0].getBoundingClientRect();
-      const a4Ratio = width / height;
-      const containerRatio = rect.width / rect.height;
-      // console.log(width, height);
-      // setScale(rect.width / a4Ratio);
-    };
-
-    updateScale();
-    const resizeObserver = new ResizeObserver(updateScale);
-    resizeObserver.observe(container);
-
-    return () => resizeObserver.disconnect();
-  }, [Document]);
 
   return (
-    <div className="bg-white h-full w-full">
+    <div className="bg-gray-100 h-full w-full flex justify-center items-start">
       <div
-        ref={containerRef}
-        className={`doc w-[${A4.width}] h-full`}
-        style={{
-          // width: A4.width,
-          // height: '1000%',
-          //   // width: A4.width,
-          // transform: `scale(0.8)`,
-          // transformOrigin: "top center",
-        }}
-        // dangerouslySetInnerHTML={{
-        //   __html: renderToString(<Document />),
-        // }}
+        className={`doc min-w-[${A4.width}] h-full bg-white overflow-y-auto`}
       >
-        {isDebugging ? (
-          renderToString(<Document />)
-        ) : (
-          <Document className="bg-white overflow-y-scroll h-full" style={{transform: 'scale(1)'}} />
-        )}
+        {isDebugging ? renderToString(<Document />) : <Document />}
       </div>
     </div>
   );
