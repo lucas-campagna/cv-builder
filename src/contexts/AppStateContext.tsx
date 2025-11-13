0e2ce9f564599baeb0b777733a62c2555e4aa9d4
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2025-11-13 20:02:38 -0300

    Update default AppStateContext

--- src/contexts/AppStateContext.tsx
+++ src/contexts/AppStateContext.tsx
@@ -1,23 +1,35 @@
 import { createContext, useState } from "react";
 
-export const AppStateContext = createContext({
-  fontSize: 14,
+const appStateDefaultValues = {
+  fontSize: 12,
   onePage: true,
   isDebugging: false,
-  toggleDebug: () => { },
-  toggleOnePage: () => { },
-  setFontSize: (_: number) => {}
-})
+  toggleDebug: () => {},
+  toggleOnePage: () => {},
+  setFontSize: (_: number) => {},
+};
 
+export const AppStateContext = createContext(appStateDefaultValues);
 
 function AppStateContextProvider({ children }: { children: React.ReactNode }) {
-  const [onePage, setOnePage] = useState(true);
-  const [isDebugging, setIsDebuggin] = useState(false);
-  const [fontSize, setFontSize] = useState(14);
-  const toggleDebug = () => setIsDebuggin((v: boolean) => !v);
+  const [onePage, setOnePage] = useState(appStateDefaultValues.onePage);
+  const [isDebugging, setIsDebugging] = useState(
+    appStateDefaultValues.isDebugging
+  );
+  const [fontSize, setFontSize] = useState(appStateDefaultValues.fontSize);
+  const toggleDebug = () => setIsDebugging((v: boolean) => !v);
   const toggleOnePage = () => setOnePage((v: boolean) => !v);
   return (
-    <AppStateContext.Provider value={{ onePage, isDebugging, fontSize, toggleDebug, toggleOnePage, setFontSize }}>
+    <AppStateContext.Provider
+      value={{
+        onePage,
+        isDebugging,
+        fontSize,
+        toggleDebug,
+        toggleOnePage,
+        setFontSize,
+      }}
+    >
       {children}
     </AppStateContext.Provider>
   );
